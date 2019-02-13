9298be0b8f64e7b0782a6c07beb91637e7bdda5a
csucom1212 <47581160+csucom1212@users.noreply.github.com>
2019-02-13 15:22:40 +0900

    Add files via upload
    I uploaded codes again.

--- project1/priority_queue.cc
+++ project1/priority_queue.cc
@@ -1,3 +1,5 @@
+// priority_queue.cc : implementation file for priority queue
+//
 #include "priority_queue.h"
 
 template <>
@@ -10,25 +12,25 @@ templatePriorityQueue<int>::~templatePriorityQueue(){
 
 template <>
 bool templatePriorityQueue<int>::empty() const{
-        return storage.empty();
+  return storage.empty();
 }
 
 template <>
 int templatePriorityQueue<int>::size() {
-        return storage.size();
+  return storage.size();
 }
 
 template <>
 const int& templatePriorityQueue<int>::top() const{
-        return storage.top();
+  return storage.top();
 }
 
 template <>
 void templatePriorityQueue<int>::pop(){
-        storage.pop();
+  storage.pop();
 }
 
 template <>
 void templatePriorityQueue<int>::push(const int& val){
-        storage.push(val);
+  storage.push(val);
 }
--- project1/priority_queue.h
+++ project1/priority_queue.h
@@ -1,15 +1,22 @@
+// priority_queue.h : header file for priority queue
+//
+#ifndef PRIORITY_QUEUE_H_
+#define PRIORITY_QUEUE_H_
+
 #include <queue>
 
 template <typename T> 
 class templatePriorityQueue {
-	private:
-		std::priority_queue<T> storage;
-	public:
-		templatePriorityQueue();
-		~templatePriorityQueue();
-		bool empty() const;
-		const T& top() const;
-		int size();
-		void push(const T&);
-		void pop();	
+ public:
+  templatePriorityQueue();
+  ~templatePriorityQueue();
+  bool empty() const;
+  const T& top() const;
+  int size();
+  void push(const T&);
+  void pop();
+ private:
+  std::priority_queue<T> storage;
 };
+
+#endif
