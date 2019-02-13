9a22d42b74af8b969bdb817285f46d22ebe83261
csucom1212 <47581160+csucom1212@users.noreply.github.com>
2019-02-13 15:03:36 +0900

    Add files via upload
    - apply the space rules
    - add description for every file
    - change order of class members

---
+++ priority_queue.h
@@ -0,0 +1,22 @@
+// priority_queue.h : header file for priority queue
+//
+#ifndef PRIORITY_QUEUE_H_
+#define PRIORITY_QUEUE_H_
+
+#include <queue>
+
+template <typename T> 
+class templatePriorityQueue {
+ public:
+  templatePriorityQueue();
+  ~templatePriorityQueue();
+  bool empty() const;
+  const T& top() const;
+  int size();
+  void push(const T&);
+  void pop();
+ private:
+  std::priority_queue<T> storage;
+};
+
+#endif
